a3286243c4e7984cab32302af2a9172f057a9132
jlazo3010 <jorge3010@ciencias.unam.mx>
2025-05-15 17:47:12 -0600

    Update Calculadora1.py

--- Calculadora1.py
+++ Calculadora1.py
@@ -901,24 +901,6 @@ else:
     except Exception as e:
         st.error(f"❌ Error al cargar la base: {e}")
 
-    # Campo de entrada para el ID del cliente a eliminar
-    st.markdown("---")
-    id_cliente_eliminar = st.text_input("Introduce el ID del cliente a eliminar:")
-
-    if st.button("❌ Eliminar Cliente"):
-        if id_cliente_eliminar:
-            try:
-                df = cargar_base()
-                if id_cliente_eliminar in df['Solicitud'].values:
-                    eliminar_registro(id_cliente_eliminar)
-                    st.success(f"✅ La solicitud {id_cliente_eliminar} ha sido eliminado.")
-                    st.rerun()  
-                else:
-                    st.error(f"No se encontró la solicitud {id_cliente_eliminar}.")
-            except Exception as e:
-                st.error(f"❌ Error al eliminar la solicitud: {e}")
-        else:
-            st.warning("Por favor, ingresa una solicitud para eliminar.")
     # Botón de cierre de sesión
     if st.sidebar.button("Cerrar Sesión"):
         st.session_state['autenticado'] = False
