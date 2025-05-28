cbc8bca607057839df0d279095213b606e3f4d67
jlazo3010 <jorge3010@ciencias.unam.mx>
2025-05-28 16:40:09 -0600

    Update Calculadora1.py

--- Calculadora1.py
+++ Calculadora1.py
@@ -30,7 +30,7 @@ import math
 import pickle 
 import json
 
-# ---------------- CONFIGURACIÓN ----------------
+'''# ---------------- CONFIGURACIÓN ----------------
 
 AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
@@ -52,7 +52,7 @@ else:
 s3 = boto3.client('s3',
     aws_access_key_id=AWS_ACCESS_KEY,
     aws_secret_access_key=AWS_SECRET_KEY
-)
+)'''
 
 # ---------------- FUNCIONES ----------------
 
