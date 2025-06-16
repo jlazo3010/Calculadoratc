38bd6adc60684de6aa81617ba670eff8e8ec2cf2
jlazo3010 <jorge3010@ciencias.unam.mx>
2025-06-16 13:58:24 -0600

    Update Calculadora1.py

--- Calculadora1.py
+++ Calculadora1.py
@@ -974,7 +974,7 @@ else:
                         st.session_state['Desicion_guardada'] = str(Desiscion)
                         st.session_state['Oferta_input'] = str(Oferta)
                         st.session_state['Oferta_final'] = Oferta_real
-                        st.session_state['Grupo_nombre'] = grupo_nombre
+                        st.session_state['Grupo_nombre'] = str(grupo_nombre)
                         
                         # Agregar bandera para indicar que se debe limpiar el formulario
                         st.session_state['limpiar_formulario'] = True
